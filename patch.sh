42e790f158a2ab35571e855781a4074a86d9198c
Te Du <tedudevelop@gmail.com>
2022-07-19 11:17:45 -0400

    fix patch number dir

--- patch.sh
+++ patch.sh
@@ -5,7 +5,7 @@ if [[ ${PATCH} = 0 ]]
     else
         echo "YES UPAD IS AVAILABLE linux_upad_tpad_${RELEASE}${PATCH}"
         mkdir linux_upad_tpad_${RELEASE}${PATCH}\
-        && curl -o linux_upad_tpad_${RELEASE}${PATCH}/linux_upad_tpad_${RELEASE}${PATCH}.zip https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo${RELEASE}_${MAJOR}_${MINOR}${PATCH}.zip\
-        && unzip -o linux_upad_tpad_${RELEASE}${PATCH}/*.zip -d version-${RELEASE}_${MAJOR}.${MINOR}${PATCH}/fls/\
+        && curl -o linux_upad_tpad_${RELEASE}${PATCH}/linux_upad_tpad_${RELEASE}${PATCH}.zip https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo${RELEASE}${PATCH}_${MAJOR}_${MINOR}${PATCH}.zip\
+        && unzip -o linux_upad_tpad_${RELEASE}${PATCH}/*.zip /geocode/ \
         && rm -r linux_upad_tpad_${RELEASE}${PATCH}
     fi
